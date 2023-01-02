505db76b4083eb56c68609f2db8b2f79913f5544
amp7368 <47309189+amp7368@users.noreply.github.com>
2023-01-01 20:36:25 -0500

    first commit

---
+++ src/main/java/com/voltskiya/chestloots/VoltskiyaPlugin.java
@@ -0,0 +1,14 @@
+package com.voltskiya.chestloots;
+
+import com.voltskiya.lib.AbstractModule;
+import com.voltskiya.lib.AbstractVoltPlugin;
+import java.util.Collection;
+import java.util.List;
+
+public class VoltskiyaPlugin extends AbstractVoltPlugin {
+
+    @Override
+    public Collection<AbstractModule> getModules() {
+        return List.of();
+    }
+}
